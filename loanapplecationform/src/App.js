236d8288059c021160bfe0d444362994187114d1
AbdulmalekAli60 <ZxshxZ123@gmail.com>
2024-04-07 09:19:02 +0300

    First Commit

---
+++ loanapplecationform/src/App.js
@@ -0,0 +1,12 @@
+import LoanForm from './LoanForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" style={{marginTop:"30px"}}>
+    <LoanForm></LoanForm>
+    </div>
+  );
+}
+
+export default App;
